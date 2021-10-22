f694279610364f9f3d01f0c28d58fbf2f45e8ba1
JerryLiao-GoSky <69893031+JerryLiao-GoSky@users.noreply.github.com>
2021-10-22 14:08:44 +0800

    feat: Add Basic button template.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = async function App(context) {
-  await context.sendText('Welcome to Bottender');
+  if (context.event.isText) {
+    await context.sendButtonTemplate("What do you want to do next?", [
+      {
+        type: "web_url",
+        url: "https://google.com",
+        webview_height_ratio: "compact",
+        title: "Google",
+      },
+      {
+        type: "web_url",
+        url: "https://yahoo.com",
+        webview_height_ratio: "compact",
+        title: "Yahoo",
+      },
+    ]);
+  }
 };
